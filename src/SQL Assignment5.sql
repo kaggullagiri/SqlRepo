ff599483da1a7d1533c59d012ea4c99779a1b10d
kaggullagiri <150141024+kaggullagiri@users.noreply.github.com>
2023-11-29 11:33:11 +0530

    Add files via upload

---
+++ src/SQL Assignment5.sql
@@ -0,0 +1,27 @@
+-- creating a table 
+create table email_signup(
+id int,
+email_id varchar(40),
+signup_date varchar(20));
+--passing values into the tables
+insert into email_signup values
+(1, 'Rajesh@Gmail.com', '2022-02-01'),
+(2, 'Rakesh_gmail@rediffmail.com', '2023-01-22'),
+(3, 'Hitest@Gmail.com', '2020-09-08'),
+(4, 'Salil@Gmmail.com', '2019-07-05'),
+(5, 'Himanshu@Yahoo.com', '2023-05-09'),
+(6, 'Hitesh@Twitter.com', '2015-01-01'),
+(7, 'Rakesh@facebook.com', null);
+-- checking table is created or not & checking values are inserted or not
+select * from email_signup;
+-- Query to find gmail accounts with latest and first signup date and difference
+
+SELECT
+    COUNT(*) AS AccountCount,
+    MAX(signup_date) AS latest_signup_date,
+    MIN(signup_date) AS first_signup_date,
+    DATEDIFF(DAY, MIN(signup_date), MAX(signup_date)) AS diff_in_days
+FROM email_signup
+WHERE LOWER(email_id) LIKE '%gmail.com';
+
+
---
+++ src/SQL Assignment6.sql
@@ -0,0 +1,46 @@
+select * from INFORMATION_SCHEMA.tables;
+--1.create a table named sales_data with columns: product_id, sale_date, and quantity_sold.
+create table sales_data(
+	product_id int,
+	sale_date date,
+	quantity_sold int);
+--2.inserting values into table
+insert into sales_data values
+(1, '2022-01-01', 20),
+(2, '2022-01-01', 15),
+(1, '2022-01-02', 10),
+(2, '2022-01-02', 25),
+(1, '2022-01-03', 30),
+(2, '2022-01-03', 18),
+(1, '2022-01-04', 12),
+(2, '2022-01-04', 22);
+--checking values inserted or not
+select * from sales_data;
+--3.assign rank by partition based on product_id and in each product id 
+--finding the lowest sold quantity
+select product_id,
+    RANK() OVER (PARTITION BY product_id ORDER BY quantity_sold) AS rank,
+    quantity_sold
+from sales_data;
+--4.retrieves the value from a previous row in the result set,
+--retrieves the value from the next row.
+select product_id,quantity_sold,
+    LAG(quantity_sold) OVER (ORDER BY sale_date) AS prev_quantity,
+    LEAD(quantity_sold) OVER (ORDER BY sale_date) AS next_quantity
+from sales_data;
+
+--5.return the first and last values in an ordered set
+
+select
+    product_id,
+    FIRST_VALUE(quantity_sold) OVER (PARTITION BY product_id ORDER BY sale_date) AS first_quantity,
+    LAST_VALUE(quantity_sold) OVER (PARTITION BY product_id ORDER BY sale_date) AS last_quantity
+from
+    sales_data;
+-- verifying table data
+select *  from sales_data;
+
+
+
+
+--
